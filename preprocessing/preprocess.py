51d9156a8142b8b5ad03ca98ab2f4bb9b95cd614
Nemish Mehta <23.nemishmehta@gmail.com>
2022-04-13 09:35:28 +0200

    First iteration of preprocessing code.

--- preprocessing/preprocess.py
+++ preprocessing/preprocess.py
@@ -3,17 +3,32 @@
 import json
 import pandas as pd
 import nltk
+import spacy
 from gensim.utils import simple_preprocess
 from gensim.models.phrases import Phrases, Phraser
-import spacy
+from functools import lru_cache
+from tqdm import tqdm
+import pickle
+
+nlp = spacy.load('en_core_web_sm', disable=['parser', 'ner'])
 
 
-def sent_to_words(sentences):
+def sent_to_words(bodies):
     """
     Function to tokenize sentences in each email body into a list of words
     """
-    for sentence in sentences:
+    for sentence in bodies:
         yield (simple_preprocess(str(sentence), deacc=True))
+    """
+    # Method 2
+    raw_words = []
+    for line in bodies:
+        index = []
+        for token in nlp(line):
+            index.append(token.text)
+        raw_words.append(index)
+    return raw_words
+    """
 
 
 def remove_stopwords(text):
@@ -21,7 +36,7 @@ def remove_stopwords(text):
     Function to remove stopwords.
     """
     stop_words = nltk.corpus.stopwords.words('english')
-    stop_words.extend(['from', 'subject', 're', 'edu', 'use'])
+    stop_words.extend(['from', 'subject', 're', 'edu', 'use', 'www'])
     return [[word for word in doc if word not in stop_words] for doc in text]
 
 
@@ -40,7 +55,6 @@ def lemmatize(texts,
     """
     Funtion to lemmatize words.
     """
-    nlp = spacy.load('en_core_web_trf', disable=['parser', 'ner'])
     texts_out = []
     for sent in texts:
         doc = nlp(" ".join(sent))
@@ -53,51 +67,37 @@ def preprocess_emails():
     """
     Main function in preprocess script to clean emails message body
     """
-    df = pd.read_csv("data/lay_k_emails.csv")
+    df = pd.read_csv("data/emails.csv")
 
-    data = df['Body'].values.tolist()
+    data_bodies = df['Body'].values.tolist()
     #print(data[:2])
 
     # Convert sentences to words --> Try to optimize this!!
-    data_words = list(sent_to_words(data[:2]))
+    data_words = list(tqdm(sent_to_words(data_bodies)))
     #print(data_words[:2])
+    print("Sentences converted to words")
 
     # Remove stop words
-    data_words_nostops = remove_stopwords(data_words)
-    #print(data_words_nostops[:2])
+    data_words_nostops = list(tqdm(remove_stopwords(data_words)))
+    #print(data_words_nostops)
+    print("Stop words removed")
 
     # Form bigrams
-    data_words_bigram = bigram(data_words_nostops)
+    data_words_bigram = list(tqdm(bigram(data_words_nostops)))
     #print(data_words_bigram[:2])
+    print("Bigrams added")
 
     # Lemmatize --> Try to optimize this!!
-    data_lemmatized = lemmatize(
-        data_words_bigram, allowed_postages=['NOUN', 'ADJ', 'VERB', 'ADV'])
+    data_lemmatized = list(
+        tqdm(
+            lemmatize(data_words_bigram,
+                      allowed_postages=['NOUN', 'ADJ', 'VERB', 'ADV'])))
 
-    print(data_lemmatized[:2])
+    #print(data_lemmatized[:2])
+    print("Lemmatization done")
 
-    #jsonString = {"data": data_lemmatized}
-
-    #with open('data/clean_body.json', 'w') as file:
-    #    json.dump(jsonString, file)
+    with open('data/word_list.pkl', 'wb') as f:
+        pickle.dump(data_lemmatized, f)
 
 
 preprocess_emails()
-"""
-def normalize_document(doc):
-    
-    # Remove stop words
-    tokens = nltk.word_tokenize(doc)
-    filtered_tokens = [token for token in tokens if token not in stopwords]
-    doc = " ".join(filtered_tokens)
-    #print(f"Doc: {doc}")
-    return doc
-
-
-normalize_corpus = np.vectorize(normalize_document)
-#print(f"Normalize Corpus: {normalize_corpus}")
-
-norm_corpus = normalize_corpus(df['Body'][0:5])
-#print(f"Norm Corpus: {norm_corpus}")
-
-"""
