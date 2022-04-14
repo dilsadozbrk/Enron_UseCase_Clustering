885ebc8ee9dafbfb93731141305accd6de30ed52
becode <fortune.tshali@gmail.com>
2022-04-14 16:49:18 +0200

    update and bugfix guy.py

--- Utils/manage.py
+++ Utils/manage.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from typing import Dict,List
+import streamlit as st
 
 
 def get_number_emails(emails:pd.DataFrame) -> Dict:
@@ -15,3 +16,22 @@ def get_number_emails(emails:pd.DataFrame) -> Dict:
         counter += 1
 
     return my_dict
+
+
+
+def open_css(path):
+    with open(path) as file:
+        st.markdown(f"<style>{file.read()}</style>",unsafe_allow_html=True)
+
+
+def get_list_email_by_token(token,emails):
+    counter = 0
+    word = token
+    list_index = []
+    for elem in emails["Keywords"]:
+        if word in emails["Keywords"].iloc[counter]:
+            list_index.append(counter)
+        counter += 1
+    return list_index
+
+
--- gui.py
+++ gui.py
@@ -1,15 +1,14 @@
 import streamlit as st
-from Utils.manage import get_number_emails
+from Utils.manage import get_number_emails,open_css,get_list_email_by_token
 from Utils.model import get_topic,run,get_tokens_by_topic
 from Utils.model import show_graph
 import pandas as pd
 from typing import List,Dict
 
-st.set_page_config(layout="wide")
 
 @st.cache
 def read_my_csv():
-    emails = pd.read_csv("data/emails.csv")   
+    emails = pd.read_csv("data/data_10k.csv")   
     return emails 
 
 @st.cache
@@ -18,73 +17,64 @@ def start_model():
     return vis
 
 
-vis = start_model()
-my_topic = vis.topic_order
 
+st.set_page_config(layout="wide")
+open_css("CSS/style.css")
 
 list_subject:List = []
 my_options:List = []
 list_emails:List = []
 
+vis = start_model()
+my_topic = vis.topic_order
 
 emails:Dict = read_my_csv()
 list_emails = get_number_emails(emails.head(100))
 list_subject = [list_emails[number]for number in list_emails]
+my_menu = ["search","graph"]
 
 
 with st.sidebar:
-    st.write("Salut lulu")
+    option_menu = st.selectbox(label="Menu",key="option-menu",options=my_menu)
+    pass
 
-st.markdown("# "+f"<p style='background-color:#2F4F4F;font-size:1.2em'>Enron - email</p>",unsafe_allow_html=True)
+if option_menu == "search":
+    open_css("CSS/style.css")
+    st.markdown("# Enron - email")
 
-st.markdown("### Select the topic")
-
-
-option = st.selectbox(label="topic number",key="first-selection",options=my_topic)
-option_token = st.multiselect(label="tokens",key="second-selection",options=get_tokens_by_topic(vis,option),default=get_tokens_by_topic(vis,option))        
-option_email = st.selectbox(label="Test email",key="test-email",options=list_subject)
-
-option_email = option_email.split(":")[0]
-option_email = int(option_email.split("°")[1])
-
-my_body = emails["Body"].iloc[option_email]
-From = emails["From"][option_email]
-Date = emails["Date"][option_email]
-To = emails["To"][option_email]
-Subject = emails["Subject"][option_email]
-
-st.markdown(f"#### Original email [{Subject}]")#change to subject
-st.markdown(f"##### From : {From}")
-st.markdown(f"##### Date : {Date}")
-st.markdown(f"##### To : {To}")
-st.markdown(f"##### Subject : {Subject}")
-st.markdown(f"##### Email")
-st.markdown(f"<p style='color:yellow'>{my_body}</p>",unsafe_allow_html=True)
+    st.markdown("### Select the topic")
 
 
-show_graph(vis)
+    option = st.selectbox(label="topic number",key="first-selection",options=my_topic)
+    option_token = st.selectbox(label="tokens",key="second-selection",options=get_tokens_by_topic(vis,option))        
+    
+
+    list_emails = get_list_email_by_token(option_token,emails)
+    
+    option_email = st.selectbox(label="select subject",key="test-email",options=list_emails)
 
 
+    #list_emails:List = []
 
-hide_st_style = """
-            <style>
-            .css-18e3th9 {
-                    padding-top: 0rem;
-                    padding-bottom: 10rem;
-                    padding-left: 5rem;
-                    padding-right: 5rem;
-                }
-               .css-1d391kg {
-                    padding-top: 3.5rem;
-                    padding-right: 1rem;
-                    padding-bottom: 3.5rem;
-                    padding-left: 1rem;
-                }
-            
-            #MainMenu {visibility: hidden;}
-            footer {visibility: hidden;}
-            header {visibility: hidden;}
-            </style>
-            """
+    #option_email = option_email.split(":")[0]
+    #option_email = int(option_email.split("°")[1])
 
-st.markdown(hide_st_style,unsafe_allow_html=True)
+    my_body = emails["Body"].iloc[option_email]
+    From = emails["From"][option_email]
+    Date = emails["Date"][option_email]
+    To = emails["To"][option_email]
+    Subject = emails["Subject"][option_email]
+
+    st.markdown(f"#### Original email [{Subject}]")#change to subject
+    st.markdown(f"##### From : {From}")
+    st.markdown(f"##### Date : {Date}")
+    st.markdown(f"##### To : {To}")
+    st.markdown(f"##### Subject : {Subject}")
+    st.markdown(f"##### Email")
+    st.markdown(my_body)
+
+elif option_menu == "graph":
+    open_css("CSS/style-graph.css")
+    show_graph(vis)
+
+
