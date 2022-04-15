03be56f4ec939e537f1d1b00f6083908c8bc8282
becode <fortune.tshali@gmail.com>
2022-04-15 09:39:34 +0200

    add new css file, add topic_dict.pkl file,add text folder

--- Utils/manage.py
+++ Utils/manage.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from typing import Dict,List
 import streamlit as st
+import re
 
 
 def get_number_emails(emails:pd.DataFrame) -> Dict:
@@ -18,8 +19,24 @@ def get_number_emails(emails:pd.DataFrame) -> Dict:
     return my_dict
 
 
+def get_dic_subject(list_emails,emails) -> Dict:
+    my_list = []
+    
+    for counter,elem in enumerate(list_emails):
+        subject = emails["Subject"].iloc[elem]
+        my_list.append(f"Email n°{elem} : {subject}")
+    my_dict = {}
+    counter = 0
 
-def open_css(path):
+    for elem in my_list:
+        my_dict[counter] = elem
+        counter += 1
+
+    return my_dict
+
+
+
+def load_css(path):
     with open(path) as file:
         st.markdown(f"<style>{file.read()}</style>",unsafe_allow_html=True)
 
@@ -35,3 +52,23 @@ def get_list_email_by_token(token,emails):
     return list_index
 
 
+def remove_nan_subject(emails):
+    emails["Subject"] = emails["Subject"].fillna("Unknown subject")
+    return emails
+
+def get_only_subject(list_subject):
+    my_list = []
+    for elem in list_subject:
+        my_list.append(list_subject[elem])
+    return my_list
+
+
+def get_index_email(option_email):  
+    option_email = option_email.split(":")[0]
+    option_email = int(option_email.split("°")[1])
+    return option_email
+
+
+def load_text(path):
+    with open(path) as file:
+        st.markdown(f"<p id='text-history'>{file.read()}</p>",unsafe_allow_html=True)
--- gui.py
+++ gui.py
@@ -1,8 +1,11 @@
 import streamlit as st
-from Utils.manage import get_number_emails,open_css,get_list_email_by_token
+from Utils.manage import get_number_emails,load_css,get_list_email_by_token
+from Utils.manage import get_dic_subject,remove_nan_subject,get_only_subject
+from Utils.manage import get_index_email,load_text
 from Utils.model import get_topic,run,get_tokens_by_topic
 from Utils.model import show_graph
 import pandas as pd
+import pickle
 from typing import List,Dict
 
 
@@ -16,56 +19,73 @@ def start_model():
     vis = run()
     return vis
 
+@st.cache
+def initiate_dataframe():
+    emails:Dict = read_my_csv()
+    list_emails = get_number_emails(emails.head(100))
+    emails = remove_nan_subject(emails)
+    return list_emails,emails
 
 
 st.set_page_config(layout="wide")
-open_css("CSS/style.css")
+load_css("CSS/style.css")
+
+with open("./data/topic_dict.pkl","rb") as file:
+    data_dict = pickle.load(file)
 
 list_subject:List = []
 my_options:List = []
 list_emails:List = []
+my_list:List = []
+menu_graph = "Graph - LDA"
+menu_investigation = "Investigation"
+menu_history = "History"
+menu_contact = "Contact"
+my_menu = [menu_investigation,menu_graph,menu_history,menu_contact]
 
 vis = start_model()
 my_topic = vis.topic_order
 
-emails:Dict = read_my_csv()
-list_emails = get_number_emails(emails.head(100))
+
+list_emails,emails = initiate_dataframe()
 list_subject = [list_emails[number]for number in list_emails]
-my_menu = ["search","graph"]
+
 
 
 with st.sidebar:
     option_menu = st.selectbox(label="Menu",key="option-menu",options=my_menu)
     pass
 
-if option_menu == "search":
-    open_css("CSS/style.css")
+if option_menu == menu_investigation:
+
+    load_css("CSS/style.css")
+
     st.markdown("# Enron - email")
+    st.markdown("### Investigation")
+    column_left,column_right = st.columns(2)
 
-    st.markdown("### Select the topic")
+    with column_left:
+        option = st.selectbox(label="topic number",key="first-selection",options=data_dict.keys())
 
-
-    option = st.selectbox(label="topic number",key="first-selection",options=my_topic)
-    option_token = st.selectbox(label="tokens",key="second-selection",options=get_tokens_by_topic(vis,option))        
-    
-
+    with column_right:
+        #list_tokens = get_tokens_by_topic(vis,option)
+        option_token = st.selectbox(label="tokens",key="second-selection",options=data_dict[option])        
+        
     list_emails = get_list_email_by_token(option_token,emails)
-    
-    option_email = st.selectbox(label="select subject",key="test-email",options=list_emails)
+    list_subject = get_dic_subject(list_emails,emails)
+    my_list = get_only_subject(list_subject)
 
+    option_email = st.selectbox(label="select subject",key="test-email",options=my_list) 
 
-    #list_emails:List = []
+    index_email = get_index_email(option_email)
 
-    #option_email = option_email.split(":")[0]
-    #option_email = int(option_email.split("°")[1])
+    my_body = emails["Body"].iloc[index_email]
+    From = emails["From"][index_email]
+    Date = emails["Date"][index_email]
+    To = emails["To"][index_email]
+    Subject = emails["Subject"][index_email]
 
-    my_body = emails["Body"].iloc[option_email]
-    From = emails["From"][option_email]
-    Date = emails["Date"][option_email]
-    To = emails["To"][option_email]
-    Subject = emails["Subject"][option_email]
-
-    st.markdown(f"#### Original email [{Subject}]")#change to subject
+    st.markdown(f"#### Original email [{Subject}]")
     st.markdown(f"##### From : {From}")
     st.markdown(f"##### Date : {Date}")
     st.markdown(f"##### To : {To}")
@@ -73,8 +93,23 @@ if option_menu == "search":
     st.markdown(f"##### Email")
     st.markdown(my_body)
 
-elif option_menu == "graph":
-    open_css("CSS/style-graph.css")
+elif option_menu == menu_graph:
+    load_css("CSS/style-graph.css")
     show_graph(vis)
+    with st.expander("Description"):
+        load_text("./text/LDA-description.txt")
 
+elif option_menu == menu_history:
+    load_css("CSS/style-history.css")
+    st.markdown("## History of Enron")
+    with st.expander("Explanation"):
+        load_text("./text/history.txt")
 
+elif option_menu == menu_contact:
+    #load_css("CSS/style.css")
+    st.markdown("## Becode Bruxelles")
+    st.markdown("## Promotion Bouman4")
+    st.markdown("### Team TensorFlow")
+    st.write("Nemish")
+    st.write("Dilsad")
+    st.markdown("#### Github")
